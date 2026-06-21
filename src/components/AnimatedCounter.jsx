6e43a1a1b8b2a36cce15eec944d064587f7acbdf
Pradnya <khobragadepradnya23@gmail.com>
2026-06-22 01:21:40 +0530

    Title animation, scroll and counter

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -1,17 +1,18 @@
 import { counterItems } from "../constants";
-import CountUp from "react-countup";
+import CountUpModule from "react-countup";
+
+const CountUp = CountUpModule.default;
 
 const AnimatedCounter = () => {
   return (
     <div id="counter" className="padding-x-lg xl:mt-0 mt-32">
       <div className="mx-auto grid-4-cols">
         {counterItems.map((item) => (
-          <div
-            key={item.label}
-            className="bg-zinc-900 rounded-lg p-10 flex flex-col justify-center"
-          >
-            <div className="counter-number text-white text-5xl font-bold mb-2">
+          <div className="bg-zinc-900 rounded-lg p-10 flex flex-col justify-center">
+            <div key={counterItems.label} className="counter-number text-white text-5xl font-bold mb-2">
+                <div className="counter-number text-white text-5xl font-bold mb-2">
               <CountUp suffix={item.suffix} end={item.value} />
+            </div>
             </div>
 
             <div className="text-white-50 text-lg">{item.label}</div>
