cd9b162609966a911b3eb3e040e3faa1ea556092
Pradnya <khobragadepradnya23@gmail.com>
2026-06-23 11:22:03 +0530

    Navbar

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -10,7 +10,7 @@ const HeroExperience = () => {
     const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
 
     return (
-        <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
+        <Canvas camera={{ position: [0, 0, 12], fov: 45 }}>
             <OrbitControls 
               enablePan={false}
               enableZoom={!isTablet}
